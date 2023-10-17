2c2db1ff7cb949ebd06295fcb909d572bae593dd
FilipposGravanis <grampouni20@gmail.com>
2023-10-17 20:29:13 +0300

    commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+function createGrid(numberofBoxes){
+    const clearButton = document.querySelector('.clear')
+    clearButton.addEventListener('click',()=>clearGrid())
+    const container = document.querySelector('.gridContainer')
+    let boxSize = (700/numberofBoxes) + "px"
+    for (let rowNumber =0 ;rowNumber<numberofBoxes;rowNumber++){
+        for (let columnNumber =0 ;columnNumber<numberofBoxes;columnNumber++){
+            let box = document.createElement('div')
+            box.style.backgroundColor = "lightgray"
+            box.style.border = "1px solid rgb(51, 51, 51)"
+            box.style.display = "inline-block"
+            box.style.height = boxSize;
+            box.style.width = boxSize;
+            box.style.flexShrink = 0
+            box.style.flexGrow = 0
+            box.addEventListener('mouseover',() =>  {
+                box.style.backgroundColor = "rgb(126, 126, 126)"
+            })
+            container.appendChild(box)
+        }
+    }
+
+
+}
+
+function clearGrid (){
+    const container = document.querySelector('.gridContainer')
+    console.log(container.childElementCount)
+    for (let box of container.children){
+        box.style.backgroundColor = "lightgray"
+    }    
+
+}
+
+createGrid(20)
+
+
